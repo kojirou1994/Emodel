95262b30c87bf4f5dd018b57702085603fa500dc
kojirou <kojirouhtc@gmail.com>
2015-10-20 12:43:03 +0800

    remove maintabbar

---
+++ emw_demo/emw_demo/Appearance.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  emw_demo
+//
+//  Created by 王宇 on 15/10/20.
+//  Copyright © 2015年 emodel. All rights reserved.
+//
+
+import Foundation
---
+++ emw_demo/emw_demo/Chat.swift
@@ -0,0 +1,9 @@
+//
+//  Chat.swift
+//  emw_demo
+//
+//  Created by 王宇 on 15/10/20.
+//  Copyright © 2015年 emodel. All rights reserved.
+//
+
+import Foundation
